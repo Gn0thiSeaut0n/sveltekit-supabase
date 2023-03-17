602c723663449fcb6096fd6aeeca5ca58945af48
Gn0thiSeaut0n <kin280000@gmail.com>
2023-03-17 10:08:24 +0900

    update cube

--- src/lib/Cubed.svelte
+++ src/lib/Cubed.svelte
@@ -4,16 +4,19 @@
 
     let showMessage = false;
     let spin = 0;
+    let scale = 0;
     
     SC.onFrame(() => {
-      spin += .01;
+      spin += .02;
+      if( scale < 2)
+        scale += .005;
     });
 </script>
   
 <button on:click={() => showMessage = true} class="relative w-full mt-8 h-64">
-  <SC.Canvas background={new THREE.Color(250, 247, 245)} >
-    <SC.Mesh geometry={new THREE.TorusKnotGeometry()} rotation={[spin, spin, spin]}/>
-    <SC.PerspectiveCamera  />
+  <SC.Canvas background={new THREE.Color(255, 255, 255)}>
+    <SC.Mesh geometry={new THREE.BoxGeometry()} rotation={[spin, spin, spin]} scale={[scale, scale, scale]}/>
+    <SC.PerspectiveCamera/>
   </SC.Canvas>
 </button>
 
